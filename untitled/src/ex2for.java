7f5bcbd3a80357134c62a4066e507698b6585b2c
PAOLA CRISTINE SILVA BASTOS <paola.bastos@senaisp.edu.br>
2026-04-29 16:37:29 -0300

    feat (ex2for):N maior e menor)

--- untitled/src/ex2for.java
+++ untitled/src/ex2for.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ex2for {
     static void main() {
+        Scanner sc = new Scanner(System.in);
+        IO.println("Me de um numero e identificaremos o Maior e Menor");
+        int N = sc.nextInt();
+        int namber = 0;
+        for ( int num = 0;num <=N; num++ ){
+            IO.println(num);
 
+        }
+        IO.println("O maior numero é " + N);
+        IO.println("O menor numero é "+namber);
     }
 }
+
+
+
+
+
+
+
+
+
