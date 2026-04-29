814466e4f697d8e170bc3fd266b26b607c9d4266
PAOLA CRISTINE SILVA BASTOS <paola.bastos@senaisp.edu.br>
2026-04-29 17:07:35 -0300

    feat (ex3for)soma dos digitos

--- untitled/src/ex3for.java
+++ untitled/src/ex3for.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ex3for {
     static void main() {
+                Scanner sc= new Scanner(System.in);
+                IO.println("Digite um número: ");
+                int numero = sc.nextInt();
+                int soma = 0;
 
-    }
-}
+                numero = (numero);
+                while (numero > 0) {
+                    soma += numero % 10;
+                    numero /= 10;
+                }
+                IO.println("A soma dos dígitos de " + numero+ " é: " + soma);
+            }
+        }
+
+
+
